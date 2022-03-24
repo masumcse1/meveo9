7ddfe964d7bd9cc5e5e4f7f06f445fa256b1c1ef
masumcse1 <optinfra.masum@gmail.com>
2022-03-25 00:30:00 +0600

    added

---
+++ meveo-admin/ejbs/src/main/java/org/meveo/api/exception/BusinessApiException.java
@@ -0,0 +1,22 @@
+package org.meveo.api.exception;
+
+
+/**
+ * @author Edward P. Legaspi
+ **/
+public class BusinessApiException  {
+
+    private static final long serialVersionUID = -5546608621039046117L;
+
+    public BusinessApiException() {
+
+    }
+
+    public BusinessApiException(String message) {
+
+    }
+
+    public BusinessApiException(Throwable e) {
+
+    }
+}
